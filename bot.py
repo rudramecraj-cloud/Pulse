0aab763ad3bd18208c545d85b02efd69bdea4485
rudramecraj-cloud <rudra.mec.raj@gmail.com>
2026-06-13 11:46:16 +0530

    Updated E-Mailing facility

--- bot.py
+++ bot.py
@@ -3,7 +3,11 @@
 #Runs - every day at 8:00 AM IST via Github actions
 
 import requests
+import smtplib
+from email.mime.text import MIMEText
+import os
 from datetime import date
+
 def get_weather(city="Thiruvananthapuram"):
     """Fetch today's weather as a one-line text summary."""
     url = f"https://wttr.in/{city}?format=3"
@@ -56,6 +60,20 @@ def run():
     with open("daily_summary.txt", "w", encoding="utf-8") as f:
         f.write(summary)
     print("Pulse ran succesfully")
+
+def send_email(summary_text):
+    sender=os.environ.get("SENDER_EMAIL")       
+    receiver=os.environ.get("RECEIVER_EMAIL") 
+    password=os.environ.get("EMAIL_PASSWORD")
+    msg=MINEText(summary_text)
+    msg["Subject"]="Pulse - Daily Summary"
+    msg["From"]=sender
+    msg["To"]=receiver
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(sender, password)
+        server.send_message(msg)
+    print("Email sent successfully")
 
 if __name__ == "__main__":    
     run()
